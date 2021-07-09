cfb55d5f950b71c926ff924ad093f1fdea91eab0
Aline Novais <54476901+astroaline@users.noreply.github.com>
2021-07-09 08:51:25 -0300

    Update input.py

--- input.py
+++ input.py
@@ -6,7 +6,7 @@ import os
 kboltz = 1.38064852e-16    # Boltzmann's constant
 amu = 1.660539040e-24      # atomic mass unit
 gamma = 0.57721
-rjup = 7.1492e9            # equatorial radius of Jupitera
+rjup = 7.1492e9            # equatorial radius of Jupiter
 rsun = 6.9566e10           # solar radius
 rearth = 6.378e8            # earth radius
 pressure_probed = 1e-2      # probed pressure in bars
@@ -21,18 +21,22 @@ m_carbon_monoxide = 28.0*amu
 
 ## Planet Data ##
 
-planet_name = 'WASP-12b'
+planet_name = 'WASP-52b'
 
-g = 977
-g_uncertainty = 67
-rstar = 1.57
-rstar_uncertainty = 0.07
-r0 = 1.79
+g = 646
+g_uperr = 45
+g_loerr = 45
+g_uncertainty = (g_uperr + g_loerr)/2
+rstar = 0.79
+rstar_uperr = 0.02
+rstar_loerr = 0.02
+rstar_uncertainty = (rstar_uperr + rstar_loerr)/2
+r0 = 1.199
 r0_uncertainty = 0.09
 
-wavelength_bins = np.array([0.838, 0.896, 0.954, 1.012, 1.070, 1.112, 1.182, 1.251, 1.320, 1.389, 1.458, 1.527, 1.597, 1.666])
-transit_depth = np.array([1.4441, 1.4422, 1.4402, 1.4428, 1.4391, 1.4386, 1.4365, 1.4327, 1.4582, 1.4600, 1.4530, 1.4475, 1.4332])
-transit_depth_error = np.array([0.0069, 0.0055, 0.0052, 0.0051, 0.0053, 0.0047, 0.0045, 0.0041, 0.0040, 0.0043, 0.0045, 0.0058, 0.0055])
+wavelength_bins = np.array([1.1107999999999998,1.1416,1.1709,1.1987999999999999,1.2257,1.2522,1.2791,1.3058,1.3321,1.3586,1.3860000000000001,1.414,1.4425,1.4718999999999998,1.5027,1.5345,1.5682,1.6042,1.6431999999999998])
+transit_depth = np.array([2.718477117350561,2.720461986046827,2.692395426210081,2.71633103482348,2.699371522240168,2.6854591342633687,2.685953812973567,2.7113144731241237,2.734573447702161,2.7754339978059117,2.7465526304776047,2.7206363146982318,2.7154493398982633,2.7041020387754977,2.708223989100348,2.6867324036147693,2.681259162249782,2.687464478204251])
+transit_depth_error = np.array([0.012273944764401792,0.009560440189967807,0.012308393828975021,0.010007946965824297,0.012573731038705637,0.010990328530552821,0.010862833736521506,0.012686953933709765,0.014366851120553534,0.012302853462452685,0.011207573135051597,0.012050238696995636,0.013560627518689413,0.013664400775793901,0.0113032052384349,0.014227050111014258,0.013246974684411633,0.016463991969084563])
 
 
 
@@ -50,7 +54,7 @@ wavenumber=True     # True if opacity given in terms of wavenumber, False if wav
 
 priors = {"T": [2800, 100], "log_xh2o": [13,-13], "log_xch4": [13,-13], "log_xco": [13,-13], "log_kappa_cloud": [14,-12],
           "log_P0": [4,-1], "log_kappa_0": [9,-10], "Q0": [99,1], "a": [3,3],
-          "log_r_c": [6,-7], "log_p_cia": [3,-3], "Rstar": [2*rstar_uncertainty,rstar-rstar_uncertainty], "G": [2*g_uncertainty,g-g_uncertainty], "line": [5,0]} # priors for all possible parameters
+          "log_r_c": [6,-7], "log_p_cia": [3,-3], "Rstar": [rstar_uperr+rstar_loerr,rstar-rstar_loerr], "G": [g_uperr+g_loerr,g-g_loerr], "line": [5,0]} # priors for all possible parameters
 
 
 
